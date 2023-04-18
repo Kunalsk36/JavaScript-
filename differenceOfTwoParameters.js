6aa3f6e0529c52b4e0c4483a2d116f75e72e961f
Kunalsk36 <114307131+Kunalsk36@users.noreply.github.com>
2023-04-18 21:05:30 +0530

    Add files via upload

---
+++ differenceOfTwoParameters.js
@@ -0,0 +1,14 @@
+// function difference(a, b) {
+//     return a-b;
+// }
+
+// console.log(difference(10, 5));
+
+function difference2(c, d) {
+    var num1 = c;
+    var num2 = d;
+    var diff = c - d;
+    console.log(diff);
+}
+
+difference2(10, 6);
